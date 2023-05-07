d5b9d3443a66e997c4ab3d61932b1c04aa3ddf78
Kim jung won <k38836919@gmail.com>
2023-05-07 10:53:04 +0900

    feat : Inmemory Bean 등록

--- gateway-service/src/main/java/com/example/gatewayservice/GatewayServiceApplication.java
+++ gateway-service/src/main/java/com/example/gatewayservice/GatewayServiceApplication.java
@@ -1,13 +1,24 @@
 package com.example.gatewayservice;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.actuate.web.exchanges.HttpExchange;
+import org.springframework.boot.actuate.web.exchanges.HttpExchangeRepository;
+import org.springframework.boot.actuate.web.exchanges.InMemoryHttpExchangeRepository;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.util.List;
 
 @SpringBootApplication
 public class GatewayServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(GatewayServiceApplication.class, args);
+	}
+
+	@Bean
+	public HttpExchangeRepository httpExchangeRepository(){
+		return new InMemoryHttpExchangeRepository();
 	}
 
 }
